625223361fc80ef46bd7a10b7a20c9685d8ead2c
hajarbabour <hajarbabour@gmail.com>
2016-04-04 10:02:58 +0100

    Added files via upload

---
+++ JenkinsDem/src/DemoJenkins/DemoJenkinsJobs.java
@@ -0,0 +1,14 @@
+package DemoJenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkinsJobs {
+
+	
+	
+
+	public void testJenkins()
+	{
+		System.out.println("Test Jenkins ");
+	}
+}
